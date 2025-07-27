135b0e4081ca9aef8f204c04d83fa3079dc2e202
Rodrigo de la Fuente <rodrigo@delafuente.email>
2025-07-27 16:29:59 +0200

    Replace Firefox with Librewolf and include Chromium, Telegram

--- modules/apps.nix
+++ modules/apps.nix
@@ -1,11 +1,13 @@
 { config, pkgs, ... }:
 
 {
-  programs.firefox.enable = true;
   environment.systemPackages = with pkgs; [
     neovim
     zed-editor
     git
     go-task
+    librewolf
+    ungoogled-chromium
+    telegram-desktop
   ];
 }
